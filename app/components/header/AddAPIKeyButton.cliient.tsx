020b09451d4e1a690d5fc49ed6991b62606a88eb
elameen <elameendaiyabu@gmail.com>
2024-10-25 15:10:36 +0000

    updated add api keys ui

--- app/components/header/AddAPIKeyButton.cliient.tsx
+++ app/components/header/AddAPIKeyButton.cliient.tsx
@@ -1,5 +1,5 @@
 import { classNames } from '~/utils/classNames';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, type ReactNode } from 'react';
 import { motion, type Variants } from 'framer-motion';
 import { Dialog, DialogButton, DialogDescription, DialogRoot, DialogTitle } from '~/components/ui/Dialog';
 import { cubicEasingFn } from '~/utils/easings';
@@ -15,7 +15,7 @@ export function AddApiKeyButton() {
 }
 
 interface ButtonProps {
-  children?: any;
+  children?: ReactNode;
   onClick?: VoidFunction;
 }
 
@@ -91,70 +91,84 @@ function ApiDialog({ open, setOpen }: { open: boolean; setOpen: any }) {
       variants={menuVariants}
       className="flex flex-col side-menu fixed top-0 w-[350px] h-full bg-bolt-elements-background-depth-2 border-r rounded-r-3xl border-bolt-elements-borderColor z-sidebar shadow-xl shadow-bolt-elements-sidebar-dropdownShadow text-sm"
     >
-      <div className="flex items-center h-[var(--header-height)]">{/* Placeholder */}</div>
-      <div className="flex-1 flex flex-col h-full w-full overflow-hidden">
-        <div className="flex-1 overflow-scroll pl-4 pr-5 pb-5">
-          <DialogRoot open={dialogContent !== null}>
-            <Dialog onBackdrop={closeDialog} onClose={closeDialog}>
-              {dialogContent?.type === 'add' && (
-                <>
-                  <DialogTitle>Add Api Keys</DialogTitle>
-                  <DialogDescription asChild className="grid gap-3">
-                    <div>
-                      <div>
-                        <label>open ai key</label>
-                        <input type="text" />
-                      </div>
-                      <div>
-                        <label>groq key</label>
-                        <input type="text" />
-                      </div>
-                      <div>
-                        <label>anthropic key</label>
-                        <input type="text" />
-                      </div>
-                      <div>
-                        <label>google generative ai key</label>
-                        <input type="text" />
-                      </div>
-                      <div>
-                        <label>ollama api base key</label>
-                        <input type="text" />
-                      </div>
-                      <div>
-                        <label>openai like base key</label>
-                        <input type="text" />
-                      </div>
-                      <div>
-                        <label>openai like key</label>
-                        <input type="text" />
-                      </div>
-                      <div>
-                        <label>mistral key</label>
-                        <input type="text" />
-                      </div>
-                    </div>
-                  </DialogDescription>
-                  <div className="px-5 pb-4 bg-bolt-elements-background-depth-2 flex gap-2 justify-end">
-                    <DialogButton type="secondary" onClick={closeDialog}>
-                      Cancel
-                    </DialogButton>
-                    <DialogButton
-                      type="primary"
-                      onClick={() => {
-                        console.log('saved api keys');
-                        closeDialog();
-                      }}
-                    >
-                      Add
-                    </DialogButton>
+      <DialogRoot open={dialogContent !== null}>
+        <Dialog className="min-w-96" onBackdrop={closeDialog} onClose={closeDialog}>
+          {dialogContent?.type === 'add' && (
+            <>
+              <DialogTitle>Add Api Keys</DialogTitle>
+              <DialogDescription asChild className="grid gap-3">
+                <div>
+                  <div className="flex justify-between">
+                    <Label>OPEN AI</Label>
+                    <Input />
                   </div>
-                </>
-              )}
-            </Dialog>
-          </DialogRoot>
-        </div>
-      </div>
+                  <div className="flex justify-between">
+                    <Label>GROQ</Label>
+                    <Input />
+                  </div>
+                  <div className="flex justify-between">
+                    <Label>ANTHROPIC</Label>
+                    <Input />
+                  </div>
+                  <div className="flex justify-between">
+                    <Label>GOOGLE GEN AI</Label>
+                    <Input />
+                  </div>
+                  <div className="flex justify-between">
+                    <Label>OLLAMA API BASE</Label>
+                    <Input />
+                  </div>
+                  <div className="flex justify-between">
+                    <Label>OPENAI LIKE BASE</Label>
+                    <Input />
+                  </div>
+                  <div className="flex justify-between">
+                    <Label>OPENAI LIKE</Label>
+                    <Input />
+                  </div>
+                  <div className="flex justify-between">
+                    <Label>MISTRAL</Label>
+                    <Input />
+                  </div>
+                </div>
+              </DialogDescription>
+              <div className="px-5 pb-4 bg-bolt-elements-background-depth-2 flex gap-2 justify-end">
+                <DialogButton type="secondary" onClick={closeDialog}>
+                  Cancel
+                </DialogButton>
+                <DialogButton
+                  type="primary"
+                  onClick={() => {
+                    console.log('saved api keys');
+                    closeDialog();
+                  }}
+                >
+                  Add
+                </DialogButton>
+              </div>
+            </>
+          )}
+        </Dialog>
+      </DialogRoot>
     </motion.div>
+  );
+}
+
+function Label({ children }: { children: ReactNode }) {
+  return (
+    <label className="text-md rounded-l-md min-w-40 h-9 px-1 py-4 border flex justify-center items-center font-black font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+      {children}
+    </label>
+  );
+}
+
+function Input({ placeholder, value }: { placeholder?: string; value?: string }) {
+  return (
+    <input
+      className="h-9 rounded-r-md border w-full bg-transparent p-4 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+      type="text"
+      placeholder={placeholder}
+      value={value}
+    />
   );
 }
